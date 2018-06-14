f0cecb3f6bd27c4329f722d88c8ade64d659175a
ACERY1 <y841034081@gmail.com>
2018-06-14 17:15:05 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Card from './components/Card'
+
+class App extends Component {
+	render() {
+		return (
+			<div style={this.styles}>
+				<Card />
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.scss'
+
+class Index extends React.Component {
+	state = {
+		url: '',
+		title: '',
+		cont: ''
+	}
+	
+	
+	render () {
+		return (
+			<div>
+			</div>
+		)
+	}
+}
+
+export default Index
