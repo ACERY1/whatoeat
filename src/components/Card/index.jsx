83dd77312d9b1944461645fede969bec63c199c5
ACERY1 <y841034081@gmail.com>
2018-06-17 11:46:45 +0800

    finish devideLine & type

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,14 +4,20 @@ import './style.scss'
 class Index extends React.Component {
 	state = {
 		url: '',
-		title: '',
-		cont: ''
+		title: '大大泡泡糖',
+		cont: '好吃又好玩'
 	}
 	
 	
 	render () {
 		return (
-			<div>
+			<div className="card l-box__verMid">
+				<div className="card-imgBox mt20">
+					<img className="card-imgBox-img" src={this.state.url} alt="FOOD"/>
+				</div>
+				<hr className="line-c1__s2"/>
+				<h5 className="t2-c1__normal mt20">{this.state.title}</h5>
+				<p className="t5-c3__normal mt10">{this.state.cont}</p>
 			</div>
 		)
 	}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from '../components/Card'
 
 export default class Home extends React.Component {
 	constructor(props) {
@@ -11,11 +12,7 @@ export default class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<p className="t1-c1__bold">hello</p>
-				<p className="t2-c2__normal mt10">hello</p>
-				<p className="t3-c3__bold">hello</p>
-				<p className="t4-c2__light">hello</p>
-				<p className="t5-c1__light">hello</p>
+				<Card />
 			</div>
 		)
 	}
